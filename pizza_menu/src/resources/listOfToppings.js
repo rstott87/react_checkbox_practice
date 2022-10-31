eef41a8b581d259f6ba17e8b0c26b25400433f5f
rstott87 <rstott87@yahoo.com>
2022-10-31 16:18:23 -0400

    removed unused object

--- pizza_menu/src/resources/listOfToppings.js
+++ pizza_menu/src/resources/listOfToppings.js
@@ -1,13 +1,5 @@
 const listOfToppings = ["Olives", "Pepperoni", "Cheese"];
 
-const listOfToppingsChecked = [
-  {
-    Olives: { checked: false },
-    Pepperoni: { checked: false },
-    Cheese: { checked: false }
-  }
-];
-
  useState({
    Olives: false,
    Pepperoni: false,
